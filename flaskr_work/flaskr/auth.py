1ca2422387095eda3f60d52c610b625503ce14ab
lel99999 <luan.le@cfpb.gov>
2022-01-18 23:08:07 -0500

    upd files

--- flaskr_work/flaskr/auth.py
+++ flaskr_work/flaskr/auth.py
@@ -21,7 +21,35 @@ def load_logged_in_user():
             'SELECT * from user WHERE id = ?', (user_id,)
         ).fetchone()
 
+@bp.route('/dashboard',methods=('GET', 'POST'))
+def dashboard():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
 
+        if not username:
+            error = 'Username is requred.'
+        elif not password:
+            error = 'Password is required.'
+
+        if error is None:
+            try:
+                db.execute(
+                    "INSERT INTO user(username,password) VALUES(?,?)",
+                    (username, generate_password_hash(password)),
+                )
+                db.commit()
+            except db.IntegrityError:
+                error = f"User {username} is already registered."
+            else:
+                return redirect(url_for("auth.login"))
+
+        flash(error)
+
+    return render_template('auth/dashboard.html')
+    
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
@@ -45,7 +73,7 @@ def register():
             except db.IntegrityError:
                 error = f"User {username} is already registered."
             else:
-                return redirect(Url_for("auth.login"))
+                return redirect(url_for("auth.login"))
 
         flash(error)
 
@@ -63,6 +91,8 @@ def login_required(view):
             return redirect(url_for('auth.login'))
 
         return view(**kwargs)
+    return wrapped_view
+
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
     if request.method == 'POST':
